c20af9c9b83d8aa3a82f13329e42f08cf9fc4dde
Norah92 <nora_altwaim@yahoo.com>
2017-04-13 20:46:30 +0300

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -518,7 +518,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var s = 256;
   var movingPizzas = document.getElementById("movingPizzas1");
   
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 20; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
